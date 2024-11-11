74972ab5c5e153c900be67d2286a6215ea275a94
Sergei Sidorkin <sergei.sidorkin@tptlive.ee>
2024-11-11 21:25:10 +0200

    Adding nodemon ( have error with require() )

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
 
+let name = "Sergey Sidorkin";
+
+const response = await fetch("https://www.google.com/");
+console.log(response);
 export default {
     entry: "./src/index.js",
     output: {
@@ -51,11 +55,14 @@ export default {
       },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.njk"
+            template: "./src/index.njk",
+            templateParameters: {
+                name // name: name
+            }
         }),
         new HtmlWebpackPlugin({
             template: "./src/about.njk",
             filename: "about.html"
         }),
     ],
-}
+};
