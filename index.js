8a9f9182ee31c947020308739b74fad6aac90656
Sergei Sidorkin <sergei.sidorkin@tptlive.ee>
2024-11-11 18:43:58 +0200

    Add chalk example

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-import common from "./common.js";
-common.hello();
-import es from "./es.js";
-es.hello();
-
-//const common = require("./common.js");
-//common.hello();
-//const es = require("./es.js").default;
-//es.hello();
+import chalk from "chalk";
+console.log(chalk.red.bgGray("Hello World!"));
