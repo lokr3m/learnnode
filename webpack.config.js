c54d836497649e0ab2408052a18705eaf263b439
Sergei Sidorkin <sergei.sidorkin@tptlive.ee>
2024-11-18 11:46:36 +0200

    add character page generation

--- webpack.config.js
+++ webpack.config.js
@@ -6,69 +6,78 @@ const __dirname = import.meta.dirname;
 
 let name = "Sergey Sidorkin";
 
-
-export default async () => {
 const response = await fetch("https://rickandmortyapi.com/api/character");
 const data = await response.json()
-const characters = data.results
+const characters = data.results;
+const pages = [];
+characters.forEach(character => {
+    let page = new HtmlWebpackPlugin({
+        template: "./src/character.njk",
+        filename: `character_${character.id}.html`,
+        templateParameters: {
+            character,
+        }
+    });
+    pages.push(page);
+});
+export default {
 
-  return {
-      entry: "./src/index.js",
-      output: {
-          filename: "main.js",
-          path: path.resolve(__dirname, "dist")
-      },
-      devServer: {
-          static: {
-              directory: path.join(__dirname, 'public'),
-          },
-          compress: true,
-          port: 9000,
-      },
-      module: {
-          rules: [
-              {
-                  test: /\.css$/i,
-                  use: ["style-loader", "css-loader"],
-              },
-              {
-                  test: /\.scss$/i,
-                  use: [
-                      "style-loader",
-                      "css-loader",
-                      {
-                          loader: "sass-loader",
-                          options: {
-                              sassOptions: {
-                                  quietDeps: true,
-                              }
-                          }
-                      }
-                  ],
-              },
-              {
-                  test: /\.njk$/,
-                  use: [
-                      {
-                          loader: 'simple-nunjucks-loader',
-                          options: {}
-                      }
-                  ]
-              }
-          ],
-      },
-      plugins: [
-          new HtmlWebpackPlugin({
-              template: "./src/index.njk",
-              templateParameters: {
-                  name, // name: name
-                  characters
-              }
-          }),
-          new HtmlWebpackPlugin({
-              template: "./src/about.njk",
-              filename: "about.html"
-          }),
-      ],
-  };
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sassOptions: {
+                                quietDeps: true,
+                            }
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.njk$/,
+                use: [
+                    {
+                        loader: 'simple-nunjucks-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.njk",
+            templateParameters: {
+                name, // name: name
+                characters
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/about.njk",
+            filename: "about.html"
+        }),
+        ...pages
+    ],
 };
