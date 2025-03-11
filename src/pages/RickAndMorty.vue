fe9a717e8e500aa622716bec5977a01a3431631d
Sergey Sidorkin <sergei.sidorkin@tptlive.ee>
2025-03-11 13:23:47 +0200

    Add infinite scroll example

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,40 +1,52 @@
 <script setup>
 import axios from 'axios';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
 import PagedPagination from '../components/PagedPagination.vue';
 
 let characters = ref([]);
-let pagination = ref([]);
+let pagination = ref({});
 let currentPage = ref(1);
 await page(currentPage.value);
 
 async function getCharacters(url) {
     let response = await axios.get(url);
     console.log(response.data);
-    characters.value = response.data.results;
+    characters.value.push(...response.data.results);
+    console.log(characters.value);
     pagination.value = response.data.info;
 }
 
-async function next() {
+async function next(){
     currentPage.value++;
     await getCharacters(pagination.value.next);
 }
 
-async function prev() {
+async function prev(){
     currentPage.value--;
     await getCharacters(pagination.value.prev);
 }
 
-async function page(page) {
+async function page(page){
     currentPage.value = page;
-    await getCharacters('https://rickandmortyapi.com/api/character?page= ' + page);
+    await getCharacters('https://rickandmortyapi.com/api/character?page=' + page);
 }
+
+onMounted(() => {
+    document.addEventListener('scroll', () => {
+        if(window.scrollY+window.innerHeight > document.body.clientHeight-300 ) {
+            if(pagination.value.next){
+                next()
+            }
+            
+        }
+        //console.log(document.body.clientHeight, window.scrollY+window.innerHeight);
+    });
+});
 
 </script>
 
 <template>
-    <PagedPagination :pagination="pagination" :current="currentPage" @next="next" @prev="prev" @page="page"></PagedPagination>
     <div class="columns is-multiline">
         <div class="column is-one-quarter" v-for="character in characters">
             <CharacterCard :character="character"></CharacterCard>
